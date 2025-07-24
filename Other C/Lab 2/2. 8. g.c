862e75451a4c2e1ec8dcc5a5b0d9762dc0f9072d
Anish3866 <anish206195@gmail.com>
2025-07-24 09:48:46 +0545

    Deleted

--- Other C/Lab 2/2. 8. g.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int x=2,z=4,y;
-	y = x>>2+z<<1;
-	printf("The value of y is %d",y);
-}
--- Rectangle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float L,B,A;
-	printf("Write the length and breathe of the rectangle.\n");
-	scanf("%f %f",&L,&B);
-	A=L*B;
-	printf("The area of the rectangle is %f\n",A);
-}
